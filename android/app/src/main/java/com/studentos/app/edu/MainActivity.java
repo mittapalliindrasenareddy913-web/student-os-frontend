4623bfbb55f935fa60e2276abff2b87e4abbe2de
mittapalliindrasenareddy913-web <mittapalliindrasenareddy913@gmail.com>
2026-08-02 14:05:22 +0530

    Configure dark background theme #0B0718 across Android styles, MainActivity, index.html, and Capacitor config to eliminate white flash/top bar. Update versionCode to 9 (v1.0.8)

--- android/app/src/main/java/com/studentos/app/edu/MainActivity.java
+++ android/app/src/main/java/com/studentos/app/edu/MainActivity.java
@@ -1,5 +1,6 @@
 package com.studentos.app.edu;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import androidx.activity.EdgeToEdge;
 import com.getcapacitor.BridgeActivity;
@@ -9,5 +10,8 @@ public class MainActivity extends BridgeActivity {
     public void onCreate(Bundle savedInstanceState) {
         EdgeToEdge.enable(this);
         super.onCreate(savedInstanceState);
+        if (this.bridge != null && this.bridge.getWebView() != null) {
+            this.bridge.getWebView().setBackgroundColor(Color.parseColor("#0B0718"));
+        }
     }
 }
